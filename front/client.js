5731a3231d9c88bd0f1f406a5798d0c6a5153398
LuisGrigore <luiscristiangrigore11@gmail.com>
2024-12-03 14:16:13 +0100

    front design finished

--- front/client.js
+++ front/client.js
@@ -1,6 +1,12 @@
 class Client{
 	async sendImage(image){
-		return 1
+		const formData = new FormData();
+        formData.append('image', image, 'canvas_image.png');
+		const response = await fetch('https://example.com/upload', {
+			method: 'POST',
+			body: formData,
+		});
+		return response
 	}
 }
 export default Client
